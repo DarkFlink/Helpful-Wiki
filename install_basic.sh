3ba8c92d47993ac593ea5940534f0a03fb219515
Yaroslav Gosudarkin <gosudarkin.yaroslav@gmail.com>
2021-08-26 21:56:58 +0300

    Update install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -9,8 +9,7 @@ sudo git config --global user.email "gosudarkin.yaroslav@gmail.com"
 # github CLI
 sudo apt-get install curl -y
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > 
-/dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh -y
 
